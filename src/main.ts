1a1de16d67d8c59873249839935894bc943ea501
erdneebatulzii23-stack <erdneebatulzii23@gmail.com>
2026-02-12 19:42:25 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideZonelessChangeDetection } from '@angular/core';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    provideZonelessChangeDetection()
+  ]
+}).catch((err) => console.error(err));
